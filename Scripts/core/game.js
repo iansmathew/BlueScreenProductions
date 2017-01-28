691f0e5060a80a5cbe2a79ecbcfe2e5dc243fa47
Ian Sebastian Mathew <iansmathew@gmail.com>
2017-01-27 20:52:14 -0500

    created optionState and creditState

--- Scripts/core/game.js
+++ Scripts/core/game.js
@@ -1,8 +1,11 @@
 //this is where we declare our game. This code loads last, but runs first
+//refer to the 'keys' for switching states
 var game = new Phaser.Game(1280, 720, Phaser.AUTO, 'gameDiv');
 
 game.state.add('boot', bootState);
 game.state.add('menu', menuState);
+game.state.add('option', optionState);
+game.state.add('credit', creditState);
 game.state.add('load', loadState);
 game.state.add('play', playState);
 

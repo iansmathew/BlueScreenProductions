f01086e193a2e60f2ecca75826d1593d56b4ba88
chrislee07 <christian.lee@georgebrown.ca>
2017-01-28 23:47:54 -0500

    game UI

--- Scripts/core/boot.js
+++ Scripts/core/boot.js
@@ -2,12 +2,17 @@
 var bootState = {
     //place menu assets to be loaded here
     preload: function(){
-
+        game.load.image('background', "././Assets/img/tempGameAssets/tempBackground.png");
+        game.load.image('singleP', "././Assets/img/tempMenuAssets/singlePlayer.png");
+        game.load.image('options', "././Assets/img/tempMenuAssets/btnHelp.png");
+        game.load.image('credits', "././Assets/img/tempMenuAssets/tempCredits.png");
+        game.load.image('exit', "././Assets/img/tempMenuAssets/btnExit.png");
     },
     //code to make assets goes here
     create: function(){
         game.physics.startSystem(Phaser.Physics.ARCADE);
         game.renderer.renderSession.roundPixels = true;
+
 
         game.state.start('menu');
     },
--- Scripts/core/menu.js
+++ Scripts/core/menu.js
@@ -2,12 +2,43 @@
 var menuState = {
     //code to make assets goes here
     create: function(){
-        game.state.start('load');//remove this code before starting menu UI
+        //game.state.start('load');//remove this code before starting menu UI
+        //adds background image to state
+        game.add.image(0, 0, 'background');
+
+        //displays name of game
+        var nameLabel = game.add.text(game.width/2, 80, 'PewPew',
+            {font: '50px Times New Roman', fill: '#ffffff' });
+        nameLabel.anchor.setTo(0.5, 0.5);
+
+        //create buttons
+        this.btnP1 = game.add.button(570, 200, 'singleP', this.player1);
+        this.btnO = game.add.button(570, 300, 'options', this.options);
+        this.btnC = game.add.button(570, 400, 'credits',this.credits);
+        this.btnE = game.add.button(570, 500, 'exit',this.exit);
+
     },
     //code to update the assets goes here //changes are reflected in game render
     update: function(){
 
     },
+
+    player1: function() {
+        game.state.start('load');
+    },
+
+    options: function() {
+
+    },
+
+    credits:function() {
+
+    },
+
+    exit:function() {
+
+    },
+
 };
 
 /* -- States below are the different menu button states -- */

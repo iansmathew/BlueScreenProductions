c35e74a858217db53f4e82c295e01ab3da62b9b1
TerryHumber <Terry.Humber@georgebrown.ca>
2017-02-17 19:10:55 -0500

    Updated menu.js credit state

---
+++ BlueScreenProductions-master/Scripts/core/game.js
@@ -0,0 +1,19 @@
+//this is where we declare our game. This code loads last, but runs first
+//refer to the 'keys' for switching states
+var game = new Phaser.Game(1280, 736, Phaser.AUTO, 'gameDiv');
+
+game.global = {
+	score1: 0,
+	score2: 0,
+};
+
+game.state.add('boot', bootState);
+game.state.add('menu', menuState);
+game.state.add('option', optionState);
+game.state.add('credit', creditState);
+game.state.add('instruction', instructionState);
+game.state.add('gameOver', gameOverState);
+game.state.add('load', loadState);
+game.state.add('play', playState);
+
+game.state.start('boot');
---
+++ BlueScreenProductions-master/Scripts/core/menu.js
@@ -0,0 +1,135 @@
+//state is the main menu state
+var menuState = {
+    //code to make assets goes here
+    create: function(){
+        //game.state.start('load');//remove this code before starting menu UI
+        //adds background image to state
+        game.add.image(0, 0, 'background');
+
+        //displays name of game
+        var nameLabel = game.add.text(game.width/2, 80, 'PewPew',
+            {font: '50px Times New Roman', fill: '#ffffff' });
+        nameLabel.anchor.setTo(0.5, 0.5);
+
+        //create buttons
+        this.btnP1 = game.add.button(570, 200, 'singleP', function () {
+            game.state.start('instruction'); //switches to Help
+        });
+        this.btnO = game.add.button(570, 300, 'options', function () {
+            //switches to Options
+        });
+        this.btnC = game.add.button(570, 400, 'credits',function () {
+            game.state.start('credit');
+        });
+        this.btnE = game.add.button(570, 500, 'exit', function () {
+            //switches to Exit
+        });
+
+    }
+
+};
+
+/* -- States below are the different menu button states -- */
+
+var instructionState = {
+    create: function () {
+        game.add.image(0, 0, 'background');
+
+        var instructions = game.add.text(500, 80, 'Instructions',
+            {font: '50px Times New Roman', fill: '#ffffff' });
+
+        var upMoveMent = game.add.text(300, 200, 'Up Arrow Key - Jump',
+            {font: '40px Times New Roman', fill: '#ffffff' });
+
+        var rMoveMent = game.add.text(300, 300, 'Right Arrow Key - Move right',
+            {font: '40px Times New Roman', fill: '#ffffff' });
+
+        var lMoveMent = game.add.text(300, 400, 'Left Arrow Key - Move left',
+            {font: '40px Times New Roman', fill: '#ffffff' });
+
+        var shoot = game.add.text(300, 500, 'Space Bar - Shoot!',
+            {font: '40px Times New Roman', fill: '#ffffff' });
+
+        this.btnPlay = game.add.button(570, 600, 'play', function () {
+            game.state.start('load'); //Switches to load state. This starts the game.
+        });
+
+    }
+};
+
+
+    var optionState = {
+    //code to create the buttons and text
+    create: function(){
+
+    },
+    //code to update the assets goes here //changes are reflected in game render
+    update: function(){
+
+    }
+};
+
+
+var creditState = {
+    //code to create the buttons and text
+    create: function(){
+        game.add.image(0,0,'background');
+         game.add.text(300,50,'                      PEW PEW\n               ' +
+            '              BY\n     BLUE SCREEN PRODUCTIONS',
+            {font: '40px Times New Roman',fontWeight: 'bold',fill: '#000000'});
+        game.add.text(275,265,'Producer                                       Cory Ronald',
+            {font: '30px Times New Roman',fontWeight: 'bold',fill: '#000000'});
+        game.add.text(275,305,'Design Lead                                  Diego Camacho',
+            {font: '30px Times New Roman',fontWeight: 'bold',fill: '#000000'});
+        game.add.text(275,345,'Engineer Lead                              Ian Sebastion Mathew',
+            {font: '30px Times New Roman',fontWeight: 'bold',fill: '#000000'});
+        game.add.text(275,385,'Assets Lead                                   Jonathan Baker',
+            {font: '30px Times New Roman',fontWeight: 'bold',fill: '#000000'});
+        game.add.text(275,425,'UI Lead                                         Chris Lee',
+            {font: '30px Times New Roman',fontWeight: 'bold',fill: '#000000'});
+        game.add.text(275,465,'Sound Lead                                   Matthew Gordon',
+            {font: '30px Times New Roman',fontWeight: 'bold',fill: '#000000'});
+        game.add.text(275,505,'QA Lead                                       Terry Humber',
+            {font: '30px Times New Roman',fontWeight: 'bold',fill: '#000000'});
+        game.add.text(275,545,'Development Lead                       Colin Pugh',
+            {font: '30px Times New Roman',fontWeight: 'bold',fill: '#000000'});
+        game.add.text(275,585,'Gameplay Lead                            Antonio Yumbla',
+            {font: '30px Times New Roman',fontWeight: 'bold',fill: '#000000'});
+        game.add.text(275,625,'Content Lead                                Jason Lin',
+            {font: '30px Times New Roman',fontWeight: 'bold',fill: '#000000'});
+
+
+
+
+    },
+    //code to update the assets goes here //changes are reflected in game render
+    update: function(){
+
+    }
+};
+
+
+
+/* This is not a part of menu. State is shown when both players die*/
+var gameOverState = {
+    create: function () {
+        game.add.image(0, 0, 'background');
+
+        var gameOver = game.add.text(550, 300, 'GAME OVER!',
+            {font: '40px Times New Roman', fill: '#ffffff' });
+
+        this.btnPlayA = game.add.button(500, 600, 'playAgain', function () {
+            game.state.start('play'); //Starts the game again
+        });
+        this.btnE = game.add.button(700, 610, 'exit',function () {
+            game.state.start('boot'); //Goes to menuState
+        });
+		
+		 this.scoreCounter1 = game.add.text(70, 10,'P1 Score: ' + game.global.score1,
+            {font: '20px Times New Roman', fill: '#ffffff' });
+
+        this.scoreCounter2 = game.add.text(1120, 10,'P2 Score: ' + game.global.score2,
+            {font: '20px Times New Roman', fill: '#ffffff' });
+    }
+};
+
